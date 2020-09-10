a39aa37ec427ccbe8d4b8e3856c3df04edc87231
Leandro Gargantini <leandrofgargantini@gmail.com>
2020-09-10 10:31:30 -0300

    add dummy tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-//it should render properly
-test("Renders Learn React Link", () => {
+//it should show header component
+test("Renders Header Component", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/TEST PAGE/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-//it should show header component
-
 //it should show left nav component
-
+test("Renders Left Nav Component", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Home/i);
+  expect(linkElement).toBeInTheDocument();
+});
 //it should show product Components
+test("Renders Products Component", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Sporting Goods/i);
+  expect(linkElement).toBeInTheDocument();
+});
